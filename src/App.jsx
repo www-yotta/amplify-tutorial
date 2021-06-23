b6c960a4fed5703da6d72b79980f3465f4f5c440
www-yotta <www.yotta@gmail.com>
2021-06-24 00:33:39 +0900

    チュートリアルの残りを実施

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Amplify, { API, graphqlOperation } from "aws-amplify";
 import { createTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
@@ -95,4 +96,4 @@ const styles = {
   },
 };
 
-export default App;
+export default withAuthenticator(App);
